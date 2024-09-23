a3c1562108165a1adc15c912226e64f0eff41556
RyoTani0210 <ryo.taniguchi@gmail.com>
2024-09-23 18:59:51 +0900

    モックづくり途中

--- hellocucumber/src/test/java/hellocucumber/checks/BookingApiIT.java
+++ hellocucumber/src/test/java/hellocucumber/checks/BookingApiIT.java
@@ -5,13 +5,31 @@ import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import hellocucumber.payloads.*;
 import hellocucumber.requests.*;
 import io.restassured.response.Response;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;//WireMock導入
+
+
 public class BookingApiIT{
+    //モック起動
+    private static WireMockServer authMock;
+    @BeforeAll
+    public static void setupMock(){
+        //3004番ポートにモックサーバを立てる
+        authMock = new WireMockServer(WireMockConfiguration.options().port(3004));
+        authMock.start();
+    }
+    @AfterAll
+    public static void killMock(){
+        authMock.stop();
+    }
+
     @Test
     public void getBookingSummaryShouldReturn200(){
         // GETリクエスト
@@ -42,43 +60,58 @@ public class BookingApiIT{
         assertEquals(201, response.getStatusCode());
     }
 
-    @Test
-    public void deleteBookingReturns202(){
-        //予約の削除のテスト
-        //リクエストの結合・パースをする
+    // @Test
+    // public void deleteBookingReturns202(){
+    //     //予約の削除のテスト
+    //     //リクエストの結合・パースをする
 
-        BookingDates dates = new BookingDates(
-            LocalDate.of(2021, 2, 1),
-            LocalDate.of( 2021, 2, 3)
-        );
-        Booking payload = new Booking(
-            1,
-            "Mark",
-            "Hunter",
-            200,
-            true,
-            dates,
-            "a cup of coffee"
-        );
+    //     BookingDates dates = new BookingDates(
+    //         LocalDate.of(2021, 2, 1),
+    //         LocalDate.of( 2021, 2, 3)
+    //     );
+    //     Booking payload = new Booking(
+    //         1,
+    //         "Mark",
+    //         "Hunter",
+    //         200,
+    //         true,
+    //         dates,
+    //         "a cup of coffee"
+    //     );
 
-        Response bookingResponse = BookingApi.postBooking(payload);
-        BookingResponse createBookingResponse = bookingResponse.as(BookingResponse.class);
+    //     Response bookingResponse = BookingApi.postBooking(payload);
+    //     BookingResponse createBookingResponse = bookingResponse.as(BookingResponse.class);
         
-        //認証情報を作成
-        Auth auth  = new Auth("admin", "password");
+    //     //認証情報を作成
+    //     Auth auth  = new Auth("admin", "password");
 
-        //認証をとる
-        Response authResponse = AuthApi.postAuth(auth);//ここがAuthAPIの死活に依存している
-        String authToken = authResponse.getCookie("token");
+    //     //認証をとる
+    //     Response authResponse = AuthApi.postAuth(auth);//ここがAuthAPIの死活に依存している
+    //     String authToken = authResponse.getCookie("token");
 
-        Response deleteResponse = BookingApi.deleteBooking(createBookingResponse.getBookingid(), authToken);
-        assertEquals(202, deleteResponse.getStatusCode());
-    }
+    //     Response deleteResponse = BookingApi.deleteBooking(createBookingResponse.getBookingid(), authToken);
+    //     assertEquals(202, deleteResponse.getStatusCode());
+    // }
 
     @Test
     public void deleteBookingReturns202WithMocks(){
 
         //テスト準備
+        authMock.stubFor(
+            post("/auth/validate")
+            .withRequestBody(equalToJson("{\"token\": \"dummytoken\"}"))
+            .willReturn(aResponse().withStatus(200))
+            .withHeader("Content-Type", "application/json")
+            .withBody("{ \"message\": \"Token validated successfully.\" }")
+        );
+
+
+        authMock.stubFor(options(urlEqualTo("/booking"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")));
+
+
         //ヘッダ組み立て
         BookingDates dates = new BookingDates(
             LocalDate.of(2021, 2, 1),
@@ -100,7 +133,7 @@ public class BookingApiIT{
         
         //テスト
         //予約情報削除
-        Response deleteResponse = BookingApi.deleteBooking(createBookingResponse.getBookingid(), "faketoken");
+        Response deleteResponse = BookingApi.deleteBooking(createBookingResponse.getBookingid(), "dummytoken");
         assertEquals(202, deleteResponse.getStatusCode());
     }
 }
--- hellocucumber/src/test/java/hellocucumber/requests/BaseApi.java
+++ hellocucumber/src/test/java/hellocucumber/requests/BaseApi.java
@@ -3,5 +3,6 @@ package hellocucumber.requests;
 public class BaseApi {
     protected static final String baseUrl = 
     // "https://automationtesting.online/";
-    "http://localhost:8080/";
+    // "http://localhost:8080/";
+    "http://localhost:3000/";//モック用
 }
